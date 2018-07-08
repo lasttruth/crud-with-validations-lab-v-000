78c2a2a632752abf2c2ec874b72826745a0f82a9
Robin Doirin <robindoirin@gmail.com>
2018-07-08 18:22:30 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,8 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true 
+  validates :title, presence: true
   validates :title, uniqueness: {
     scope: [:release_year, :artist_name],
     message: "cannot be repeated by the same artist in the same year"
   }
+  validates :released, inclusopm: {in: [true, false] }
 end
