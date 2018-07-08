912896be3f3f7f3f985810ee52be67800423a96e
Robin Doirin <robindoirin@gmail.com>
2018-07-08 18:40:51 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,7 +5,7 @@ class Song < ActiveRecord::Base
     message: "cannot be repeated by the same artist in the same year"
   }
 
-  validates :released, inclusopm: {in: [true, false] }
+  validates :released, inclusion: {in: [true, false] }
   validates :artist_name, presence: true
 
   ith_options if: :released? do |song|
